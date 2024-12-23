05cc7e76457c29d95af094212b40e9697022bb10
YSShalini <125855192+YSShalini@users.noreply.github.com>
2024-12-23 22:11:06 +0530

    Time: 0 ms (100%), Space: 44.5 MB (44.83%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -22,14 +22,10 @@ class Solution {
                 return 0;
             }
             int left=dfs(root.left);
-            if (left==-1)return -1;
-
             int right=dfs(root.right);
-            if(right==-1)return -1;
-
-            if(Math.abs(left-right)>1)
-            return -1;
-
+            if(Math.abs(left-right)>1 || left==-1 ||right==-1){
+                return -1;
+            }
             return 1+Math.max(left,right);
         }
     }
