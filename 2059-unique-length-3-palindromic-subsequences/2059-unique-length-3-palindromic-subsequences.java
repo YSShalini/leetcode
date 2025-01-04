5847eb6572157ca75b5f4ce99802996523a581a3
YSShalini <125855192+YSShalini@users.noreply.github.com>
2025-01-04 23:49:03 +0530

    Time: 165 ms (75.13%), Space: 45.5 MB (60.21%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
@@ -1,24 +1,26 @@
 class Solution {
     public int countPalindromicSubsequence(String s) {
-        HashMap<Character,List<Integer>>map=new HashMap<>();
+        int[]first=new int[26];
+        int[]last=new int[26];
+        Arrays.fill(first,-1);
+        Arrays.fill(last,-1);
         for(int i=0;i<s.length();i++){
-            char c=s.charAt(i);
-            if(!map.containsKey(c)){
-                if(s.indexOf(c)!=s.lastIndexOf(c)){
-                map.put(c,Arrays.asList(s.indexOf(c),s.lastIndexOf(c)));
-                }
+            int v=s.charAt(i)-'a';
+            if(first[v]==-1){
+                first[v]=i;
+            }
+            last[v]=i;
+        }
+        int c=0;
+        for(int i=0;i<26;i++){
+            if(first[i]!=-1&&first[i]<last[i]){
+             HashSet <Character> res=new HashSet<>();
+             for(int j=first[i]+1;j<last[i];j++){
+                res.add(s.charAt(j));
+             }
+             c+=res.size();
             }
         }
-        HashSet <String> res=new HashSet<>();
-        for(char c:map.keySet()){
-           List<Integer>index=map.get(c);
-           int start=index.get(0);
-           int last=index.get(1);
-           for(int i=start+1;i<last;i++){
-            res.add(""+c+s.charAt(i)+c);
-        }
-        
-    }
-    return res.size();
+    return c;
     }
 }
