e4238a9e7c8910bbbb4da50f3559750c2091f5c5
YSShalini <125855192+YSShalini@users.noreply.github.com>
2025-02-14 11:58:46 +0530

    Time: 3 ms (2.09%), Space: 10.3 MB (71.71%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.c
+++ 0048-rotate-image/0048-rotate-image.c
@@ -1,16 +1,17 @@
 void rotate(int** matrix, int matrixSize, int* matrixColSize) {
-    for(int i=0;i<matrixSize;i++){
-        for(int j=i+1;j<matrixSize;j++){
-            int t=matrix[i][j];
-            matrix[i][j]=matrix[j][i];
-            matrix[j][i]=t;
+    int m=matrixSize;
+    for(int i=0;i<m;i++){
+        for(int j=i+1;j<m;j++){
+             int t=matrix[i][j];
+             matrix[i][j]=matrix[j][i];
+             matrix[j][i]=t;
         }
     }
-    for(int i=0;i<matrixSize;i++){
-        for(int j=0;j<matrixSize/2;j++){
-            int t=matrix[i][j];
-            matrix[i][j]=matrix[i][matrixSize-1-j];
-            matrix[i][matrixSize-1-j]=t;
+    for(int i=0;i<m;i++){
+        for(int j=0;j<m/2;j++){
+             int t=matrix[i][m-j-1];
+             matrix[i][m-j-1]=matrix[i][j];
+             matrix[i][j]=t;
         }
     }
 }
