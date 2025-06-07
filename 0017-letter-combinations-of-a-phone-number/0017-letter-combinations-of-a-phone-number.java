bddc6bffe1080a462e77e48d88f7ee07371370aa
SHALINI Y S <shaliniys.22cse@kongu.edu>
2025-06-07 20:25:55 +0530

    Time: 0 ms (100%), Space: 42.4 MB (26.18%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,24 +1,26 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
-        List<String>s=new ArrayList<>();
-        StringBuilder ns=new StringBuilder();
-        if(digits.length()==0){return s;}
-        String[] map={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-        backtrack(0,digits,map,s,ns);
-        return s;
-
-        }
-    public void backtrack(int idx,String digits,String[] map,List<String>s,StringBuilder ns){
-        if(idx==digits.length()){
-            s.add(ns.toString());
+        List<String>res=new ArrayList<>();
+      int n=digits.length();
+      if(n==0){return res;}
+      String[] arr=new String[19];
+      arr[0]="";arr[1]="";arr[2]="abc";arr[3]="def";arr[4]="ghi";arr[5]="jkl";arr[6]="mno";
+      arr[7]="pqrs";arr[8]="tuv";arr[9]="wxyz";
+      StringBuilder ns=new StringBuilder();
+      dfs(res,digits,arr,0,ns);
+      return res;
+    }
+    void dfs(List<String>res,String digits,String[]arr,int start,StringBuilder ns){
+          if(start==digits.length()){
+            res.add(ns.toString());
             return;
-        }
-        String l=map[digits.charAt(idx)-'0'];
-        for(char c:l.toCharArray()){
-            ns.append(c);
-            backtrack(idx+1,digits,map,s,ns);
-            ns.deleteCharAt(ns.length()-1);
-        }
-        
+         }
+         int num=digits.charAt(start)-'0';
+            String ans=arr[num];
+         for(int i=0;i<ans.length();i++){  
+            ns.append(ans.charAt(i));    
+         dfs(res,digits,arr,start+1,ns);
+         ns.deleteCharAt(ns.length()-1);
     }
-    }
+}
+}
