e88ce71b29b5b2f41db4f6ef6370a94321395aee
SHALINI Y S <shaliniys.22cse@kongu.edu>
2025-11-01 14:08:29 +0530

    Time: 0 ms (100%), Space: 43.8 MB (5.63%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,20 +1,16 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int r=matrix.length,c=matrix[0].length;
-        int l=0,ri=r*c-1;
-        while(l<=ri){
-            int mid=l+(ri-l)/2;
-            int midv=matrix[mid/c][mid%c];
-            if(midv==target){
-                return true;
-            }
-            else if(midv<target){
-                l=mid+1;
-            }
-            else {
-                ri=mid-1;
-            }
-        }
-        return false;
+       int row=matrix.length,col=matrix[0].length;
+       int l=0,r=row*col-1;
+       while(l<=r){
+         int mid=l+(r-l)/2;
+         int ro=mid/col;
+         int c=mid%col;
+         int element=matrix[ro][c];
+         if(element<target){l=mid+1;}
+         else if(element>target){r=mid-1;}
+         else{return true;}
+       }
+       return false;
     }
 }
