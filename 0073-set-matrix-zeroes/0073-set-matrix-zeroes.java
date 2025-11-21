11370c4f4f0a96548b810de40d4501a06a4d83dc
SHALINI Y S <shaliniys.22cse@kongu.edu>
2025-11-21 10:12:39 +0530

    Time: 2 ms (15.97%), Space: 47.4 MB (6.33%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,25 +1,23 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int m=matrix.length,n=matrix[0].length;
-        int[]rowarray=new int[m];
-        int[]colarray=new int[n];
-        Arrays.fill(rowarray,-1);
-        Arrays.fill(colarray,-1);
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(matrix[i][j]==0){
-                   rowarray[i]=1;
-                   colarray[j]=1;
-                }
+       HashSet<Integer>row=new HashSet<>();
+       HashSet<Integer>col=new HashSet<>();
+       int m=matrix.length,n=matrix[0].length;
+       for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(matrix[i][j]==0){
+                row.add(i);
+                col.add(j);
             }
         }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(rowarray[i]==1||colarray[j]==1){
-                    matrix[i][j]=0;
-                  
-                }
+       }
+       for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(row.contains(i)||col.contains(j)){
+                matrix[i][j]=0;
             }
         }
+       }
+    
     }
 }
